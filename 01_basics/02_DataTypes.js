af66a5a3ba9a6cda06d3d8a48ffe0ee52e5cf474
RajdeepGupta07 <guptarajdeep2003@gmail.com>
2024-10-19 10:11:57 +0530

    Create 02_DataTypes.js

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as a newer version
+
+// alert("Hello") // we are suing nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Rajdeep");
+let name = "Rajdeep";
+let age = 21;
+let isLoggedIn = false;
+let state;
+
+//number => 2 to power 53
+//bignit
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+//symbol => unique 
+
+//objects
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
