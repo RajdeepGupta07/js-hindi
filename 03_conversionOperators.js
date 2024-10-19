b05817c80a87b0643d87697f6592653db2b6177c
RajdeepGupta07 <guptarajdeep2003@gmail.com>
2024-10-19 10:33:48 +0530

    Conversion of Datatype

---
+++ 03_conversionOperators.js
@@ -0,0 +1,30 @@
+let score=null;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc" => NaN  
+// true => 1
+//false => 0
+ 
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => ture
+// 0 => false
+// "" => false
+// "Rajdeep" => true
+ 
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(someNumber) 
+console.log(typeof someNumber);
+
