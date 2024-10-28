51f674c2b104ba6110dcb7461cb2f71731b33037
RajdeepGupta07 <guptarajdeep2003@gmail.com>
2024-10-28 22:03:28 +0530

    Update 03_conversionOperators.js

--- 03_conversionOperators.js
+++ 03_conversionOperators.js
@@ -1,30 +1,66 @@
-let score=null;
+let score = null;
 
 console.log(typeof score);
-console.log(typeof (score));
+console.log(typeof score);
 
-let valueInNumber=Number(score)
-console.log(typeof (valueInNumber));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 //"33"=> 33
-//"33abc" => NaN  
+//"33abc" => NaN
 // true => 1
 //false => 0
- 
-let isLoggedIn=""
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 
 // 1 => ture
 // 0 => false
 // "" => false
 // "Rajdeep" => true
- 
-let someNumber=33
 
-let stringNumber=String(someNumber)
+let someNumber = 33;
 
-console.log(someNumber) 
+let stringNumber = String(someNumber);
+
+console.log(someNumber);
 console.log(typeof someNumber);
+
+// operations
+
+let value = 3;
+let negValue = -value;
+console.log(value);
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2/2);
+// console.log(2**2)
+// console.log(2%2)
+
+// let str1="hello"
+// let str2="Rajdeep"
+// let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
 
