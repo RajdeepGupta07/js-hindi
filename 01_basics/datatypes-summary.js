df5481f9a7b811aea605492b38897237e23d526c
RajdeepGupta07 <guptarajdeep2003@gmail.com>
2024-10-29 21:42:37 +0530

    dataTypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 types : String,Number, Boolean , null, undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedI=false
+const outTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+const bigNumber=76764886459673n
+
+
+// Reference(Non-premitive)
+
+// Arrays, Objects, Function
+
+const heros=["shaktiman", "naagraj", "doga"];
+let myObj= {
+    name:"Rajdeep",
+    age: 22,
+}
+const myFunction=function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof bigNumber)
